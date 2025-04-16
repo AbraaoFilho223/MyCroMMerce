d11dfeed6d7e4a7a5abc1d1b6c9032cd137f121f
Abraão Filho <abraaofilho223@hotmail.com>
2025-04-16 00:29:55 -0300

    Swagger fix

--- api-gateway/src/main/java/com/mycrommerce/api_gateway/config/WebConfig.java
+++ api-gateway/src/main/java/com/mycrommerce/api_gateway/config/WebConfig.java
@@ -9,6 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/swagger-ui/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
--- order-service/src/main/java/com/mycrommerce/order_service/config/WebConfig.java
+++ order-service/src/main/java/com/mycrommerce/order_service/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5173")  // Origem do frontend
-            .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
-            .allowedHeaders("*");  // Cabeçalhos permitidos
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("*");
     }
 }
